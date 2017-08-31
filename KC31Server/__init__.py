fe5f009cdde7bff2632a2f804460161d374bdf5e
Elliot Woods <elliot.woods@gmail.com>
2017-08-31 04:47:23 +0100

    refactoring - run.bat is now active

--- KC31Server/__init__.py
+++ KC31Server/__init__.py
@@ -1,2 +1,5 @@
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask(__name__,
+            static_url_path='', 
+            template_folder='templates')
--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
-set FLASK_APP=main.py
+pip install -e .
+set FLASK_APP=KC31Server
 set FLASK_DEBUG=1
 flask run --host=0.0.0.0
